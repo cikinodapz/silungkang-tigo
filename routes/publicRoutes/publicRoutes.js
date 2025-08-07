ab9e2461e8b187d9b984ce10da6877346b698041
daffaagustians <dafagustiansaadi12@gmail.com>
2025-08-07 14:18:32 +0700

    berita

--- routes/publicRoutes/publicRoutes.js
+++ routes/publicRoutes/publicRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getPopulationStats } = require('../../controllers/publicController/public');
-const { getAllBerita } = require('../../controllers/beritaControllers/berita');
+const { getAllBerita, getAllKategoriBerita, getSampul, getBerita } = require('../../controllers/beritaControllers/berita');
 const { getAllAPBDes } = require('../../controllers/APBDesController/apbd');
 const router = express.Router();
 
@@ -9,6 +9,9 @@ router.get('/getPopulationStats', getPopulationStats);
 router.get('/getAllBerita', getAllBerita); // Endpoint to get all news
 router.get('/getAllAPBDes', getAllAPBDes); // Endpoint to get all APBDes
 
-
+// Berita Routes
+router.get('/getAllKategoriBerita', getAllKategoriBerita); // Endpoint to get all news categories
+router.get('/getSampul/:type/:filename', getSampul); // Endpoint to view news cover image
+router.get('/getBerita/:id', getBerita); // Endpoint to get all
 
 module.exports = router;
