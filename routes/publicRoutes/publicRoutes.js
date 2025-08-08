9f6c34f8973f62666f006fc2602b230056fb61c1
daffaagustians <dafagustiansaadi12@gmail.com>
2025-08-08 10:43:17 +0700

    produk hukum

--- routes/publicRoutes/publicRoutes.js
+++ routes/publicRoutes/publicRoutes.js
@@ -3,6 +3,7 @@ const { getPopulationStats } = require('../../controllers/publicController/publi
 const { getAllBerita, getAllKategoriBerita, getSampul, getBerita } = require('../../controllers/beritaControllers/berita');
 const { getAllAPBDes } = require('../../controllers/APBDesController/apbd');
 const { getAllPotensiDesa, getFotoPotensiDesa, getPotensiDesa } = require('../../controllers/potensiDesaController/potensiDesa');
+const { getAllProdukHukum, getFileProdukHukum } = require('../../controllers/produkHukumController/produkHukum');
 const router = express.Router();
 
 // Public Routes
@@ -19,6 +20,10 @@ router.get('/getBerita/:id', getBerita); // Endpoint to get all
 router.get("/potensiDesa", getAllPotensiDesa);
 router.get("/getPotensiDesa/:id", getPotensiDesa);
 router.get("/foto-potensi-desa/:type/:filename", getFotoPotensiDesa); // Endpoint to view Potensi Desa photo
+
+//Produk Hukum Routes
+router.get('/getAllProdukHukum', getAllProdukHukum); // Endpoint to get all product laws
+router.get('/getFileProdukHukum/:type/:filename', getFileProdukHukum); // Endpoint to view product law file
 
 
 
